12744e296759f7e8a1aeb512c1e980f7c473ca07
AdrienMoEpsic <adrien.monnard@eduvaud.ch>
2025-05-05 20:43:43 +0200

    salut c'est le sixieme test

--- app-adrien.py
+++ app-adrien.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Je fais mon cinqueme test!"
+    return "Je fais mon sixieme test!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
