c3de95ae412e17fa7360b541bb46d3af2836ec0b
bananas869 <42307966+bananas869@users.noreply.github.com>
2025-05-12 14:26:33 +0200

    Update app-adrien.py

--- app-adrien.py
+++ app-adrien.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 def hello():
     return "Je fais mon examen 2 !"
     return "Arthur aussi !"
+    return "et diogo woah !"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
