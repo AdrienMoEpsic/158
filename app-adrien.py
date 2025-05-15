c5f82a0201ff604f2a9c81cf61ac4ac2f90b36b8
Bessos <besim_1510@hotmail.com>
2025-05-15 08:41:44 +0200

    Update app-adrien.py

--- app-adrien.py
+++ app-adrien.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello():
 
     return "voici le test de la branche (Adrien)2 !"
-
+    return "t'as pas une clope ?"
     return "Je fais mon examen 2 ! change diogo"
     return "Je fais mon examen 2 !"
     return "test Arthur"
