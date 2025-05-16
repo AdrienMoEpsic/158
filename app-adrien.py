3cafedf04ae24a0803d45d67781db70bebe38f2e
Haeween <monia.admane@hotmail.com>
2025-05-16 17:34:11 +0200

    pitie

--- app-adrien.py
+++ app-adrien.py
@@ -13,7 +13,7 @@ def hello():
     return "test test Patrick"
     return "Deuxième ligne - Diogo"
     return "JE VEUX TERMNIER CETTE DEUXIEME ANNEE ALED - Monia"
-    return "JE VEUX TERMINER CETTE DEUXIEME ANNEE ALED from CLI - Monia"
+    return "JE VEUX TERMINER CETTE DEUXIEME ANNEE ALED from CLI pitié - Monia"
 
 
 if __name__ == '__main__':
